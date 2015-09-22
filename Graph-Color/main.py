0e770699241852f8d3856a9238a3599b3302b6f8
Guilherme Zanardo Borduchi <guilherme.borduchi@usp.br>
2015-09-22 16:18:10 -0300

    Verificação Adiante

--- Graph-Color/main.py
+++ Graph-Color/main.py
@@ -1,47 +1,75 @@
 import sys
+import time
 import collections
 
 # initialize each vertex with all possible colors
 def initialize_colors(graph):
 	colors = dict()
 	for vertex in graph:
-		colors[vertex] = ['Azul', 'Vermelho', 'Verde', 'Amarelo']
+		colors[vertex] = []
 
 	return colors
 
-def backtracking_recursive(graph, graph_len, vertex_id, colors, map_colors,):
+def color_graph_recursive(graph, graph_len, vertex_id, possible_colors, used_colors, map_colors, flag):
 	if len(map_colors) == graph_len:
 		return True
 
-	vertex_adjacents = list(graph.values())[vertex_id]
 	vertex_key = list(graph.keys())[vertex_id]
+	vertex_adjacents = list(graph.values())[vertex_id]
 
-	for color in colors:
+	if flag != 'a':
+		this_used_colors = used_colors.copy()
+	else:
+		this_used_colors = used_colors
+
+	for color in possible_colors:
+		#if color in this_used_colors[vertex_key]:
+		#	print("Pulou a cor %s de %s" % (color, vertex_key))
+		#	continue
+
+		if flag != 'a':
+			this_used_colors = used_colors.copy()
+
 		available = True
 		for adjacent in vertex_adjacents:
 			if map_colors.get(adjacent) == color:
 				available = False
+				break
 
 		if available == True:
 			map_colors[vertex_key] = color
 
-			if backtracking_recursive(graph, graph_len, vertex_id + 1, colors, map_colors) == True:
+			if flag != 'a':
+				this_used_colors[vertex_key].append(color)
+				for adjacent in vertex_adjacents:
+					this_used_colors[adjacent].append(color)
+
+					if adjacent not in map_colors and len(possible_colors) == len(this_used_colors[adjacent]):
+						available = False
+
+			if available == False:
+				del map_colors[vertex_key]
+				continue
+
+			if color_graph_recursive(graph, graph_len, vertex_id + 1, possible_colors, this_used_colors, map_colors, flag) == True:
 				return True
 			else:
 				del map_colors[vertex_key]
 
 	return False
 
-def backtracking(graph):
+def color_graph(graph, flag):
 	map_colors = dict()
-	colors = ['Azul', 'Vermelho', 'Verde', 'Amarelo']
 
-	if backtracking_recursive(graph, len(graph), 0, colors, map_colors) == True:
+	possible_colors = ['Azul', 'Vermelho', 'Verde', 'Amarelo']
+	used_colors = initialize_colors(graph)
+
+	if color_graph_recursive(graph, len(graph), 0, possible_colors, used_colors, map_colors, flag) == True:
 		return map_colors
 	else:
 		return []
 
-input() # discard first line
+flag = input().split(" ")[1].split("\n")[0] # get the flag letter
 
 graph = collections.OrderedDict() # initialize the dict representing our graph
 
@@ -58,7 +86,7 @@ for line in sys.stdin:
 		else:
 			graph[src].append(dsts[i].split(".\n")[0]) # add a new edge in the graph
 
-colors = initialize_colors(graph)
-
-print(graph)
-print(backtracking(graph))
+start_time = time.time()
+print(color_graph(graph, flag))
+elapsed_time = time.time() - start_time
+print("Time taken using '%s' flag: %ss" % (flag, elapsed_time))
