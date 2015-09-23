03e57f4845cb21c620911f5be862fefa564bc612
Guilherme Zanardo Borduchi <guilherme.borduchi@usp.br>
2015-09-23 20:53:54 -0300

    Python's Style Guide Adequation

--- Graph-Color/main.py
+++ Graph-Color/main.py
@@ -2,7 +2,7 @@ import sys
 import time
 import collections
 
-# initialize each vertex with all possible colors
+# Initialize each vertex with all possible colors.
 def initialize_colors(graph):
 	colors = dict()
 	for vertex in graph:
@@ -10,8 +10,10 @@ def initialize_colors(graph):
 
 	return colors
 
-# recursive version of the coloring function
-def color_graph_recursive(graph, graph_len, vertex_id, possible_colors, used_colors, map_colors, flag):
+# Recursive version of the coloring function.
+def color_graph_recursive(
+		graph, graph_len, possible_colors, used_colors,
+		map_colors, flag='a', vertex_id=0):
 	if len(map_colors) == graph_len:
 		return True
 
@@ -22,13 +24,17 @@ def color_graph_recursive(graph, graph_len, vertex_id, possible_colors, used_col
 
 	for color in possible_colors:
 		available = True
-		for adjacent in vertex_adjacents:			#
-			if map_colors.get(adjacent) == color:	# tests the adjacent vertices to see
-				available = False					# if the current color can be used;
-				break								# if not, break to the next color
 
-		if available == True: # if the current color is available, keep going
-			map_colors[vertex_key] = color # color myself to the current color
+		# Tests the adjacent vertices to see if the current color can
+		# be used; if not, break to the next color.
+		for adjacent in vertex_adjacents:
+			if map_colors.get(adjacent) == color:
+				available = False
+				break
+
+		if available == True:
+			# Color myself to the current color.
+			map_colors[vertex_key] = color
 
 			if flag != 'a':
 				for adjacent in vertex_adjacents:
@@ -37,7 +43,9 @@ def color_graph_recursive(graph, graph_len, vertex_id, possible_colors, used_col
 							used_colors[adjacent].append(color)
 							my_colored_states.append(adjacent)
 
-							if len(possible_colors) == len(used_colors[adjacent]): # forward checking
+							# Forward checking heuristic
+							if (len(possible_colors) ==
+							    	len(used_colors[adjacent])):
 								available = False
 								break
 
@@ -47,21 +55,28 @@ def color_graph_recursive(graph, graph_len, vertex_id, possible_colors, used_col
 
 			if flag == 'c' or flag == 'd':
 				greatest = -1
-				candidate = vertex_key # only considered when graph is fully colored
+				candidate = vertex_key  # Only considered when graph
+										# is fully colored
 
 				for vertex in graph:
-					if vertex not in map_colors and len(used_colors[vertex]) > greatest:
+					if (vertex not in map_colors and 
+							len(used_colors[vertex]) > greatest):
 						greatest = len(used_colors[vertex])
 						candidate = vertex
 
 				if greatest != -1:
 					next_vertex = list(graph.keys()).index(candidate)
-				else: 			# only enters here when the current vertix is the last one to be colored
-					return True # this means it is already colored and the result is valid
+				else:
+				 	# Only enters here when the current vertix is the
+				 	# last one to be colored. This means it is already
+				 	# colored and the result is valid!
+					return True
 			else:
 				next_vertex = vertex_id + 1
 
-			if color_graph_recursive(graph, graph_len, next_vertex, possible_colors, used_colors, map_colors, flag) == True:
+			if color_graph_recursive(
+					graph, graph_len, possible_colors,
+					used_colors, map_colors, flag, next_vertex):
 				return True
 			else:
 				del map_colors[vertex_key]
@@ -71,42 +86,52 @@ def color_graph_recursive(graph, graph_len, vertex_id, possible_colors, used_col
 
 	return False
 
-# non-recursive coloring function that uses the recursive one
+# Non-recursive coloring function that uses the recursive one.
 def color_graph(graph, flag):
 	map_colors = dict()
 
 	possible_colors = ['Azul', 'Vermelho', 'Verde', 'Amarelo']
 	used_colors = initialize_colors(graph)
 
-	if color_graph_recursive(graph, len(graph), 0, possible_colors, used_colors, map_colors, flag) == True:
+	if color_graph_recursive(
+			graph, len(graph), possible_colors, used_colors,
+			map_colors, flag):
 		return map_colors
-	else:
-		return []
 
-# this function parses the input and creates the graph from it
+# Parses the input and creates the graph from it.
 def parse_input():
-	flag = input().split(" ")[1].split("\n")[0] # get the flag letter
+	flag = input().split(" ")[1].split("\n")[0]  # Grab the flag letter
 
-	graph = collections.OrderedDict() # initialize the dict representing our graph
+	# Initialize the dict representing our graph.
+	graph = collections.OrderedDict()
 
 	for line in sys.stdin:
-		elems = line.split(": ")	#
-		src = elems[0]			    #
-		dsts = elems[1].split(", ") # input parsing
+		elems = line.split(": ")	 # 
+		src = elems[0]			     # Separates the vertix
+		dsts = elems[1].split(", ")  # from its adjacents.
 
-		graph[src] = [] # initialize this vertex's adjencency list
+		graph[src] = []  # Initialize this vertex's adjencency list
 
 		for i in range(len(dsts)):
 			if i != (len(dsts) - 1):
-				graph[src].append(dsts[i]) # add a new edge in the graph
+				# Add a new edge for this vertix
+				graph[src].append(dsts[i])
 			else:
-				graph[src].append(dsts[i].split(".\n")[0]) # add a new edge in the graph
+				# Add the last edge for this vertix (ends with '\n')
+				graph[src].append(dsts[i].split(".\n")[0])
 
 	return (graph, flag)
 
-graph, flag = parse_input()
+# Main function
+def main():
+	graph, flag = parse_input()
 
-start_time = time.time()
-print(color_graph(graph, flag))
-elapsed_time = time.time() - start_time # count time for color_graph function
-print("Time taken using '%s' flag: %ss" % (flag, elapsed_time))
+	# Colors the graph and counts the total time elapsed for that
+	start_time = time.time()
+	print(color_graph(graph, flag))
+	elapsed_time = time.time() - start_time
+	
+	print("Time taken using '%s' flag: %ss" % (flag, elapsed_time))
+
+if __name__ == '__main__':
+	main()
